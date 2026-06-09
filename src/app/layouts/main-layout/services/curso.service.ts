7979403f22f310a76e82e24d1e1f0e5c6856ba5a
Kevin <kevin.dayan.lalangui@estudiante.epsum.school>
2026-06-09 18:40:16 +0200

    añadir actividad

---
+++ src/app/layouts/main-layout/services/curso.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+
+export interface CursoService {
+  id: number;
+  nombre: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class CursoService {
+  private URL = environment.apiURL;
+
+  constructor(private http: HttpClient) {
+  }
+  getActividades(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.URL}/actividad/all`);
+  }
+  addActividad(datos: any): Observable<any> {
+    return this.http.post<any>(`${this.URL}/actividad/add`, datos);
+  }
+  deleteActividad(id:string):Observable<any>{
+    return this.http.delete<any>(`${this.URL}/actividad/delete/${id}`)
+  }
+}
